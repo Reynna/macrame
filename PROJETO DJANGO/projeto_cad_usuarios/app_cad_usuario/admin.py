dc345f974af765f1d4acf4158b3ac2b6e797aa55
Reynna <114556579+Reynna@users.noreply.github.com>
2025-05-19 17:56:18 -0300

    Delete PROJETO DJANGO/projeto_cad_usuarios directory

--- PROJETO DJANGO/projeto_cad_usuarios/app_cad_usuario/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Estoque, Projeto, Historico
-
-# Registra os modelos no painel administrativo
-admin.site.register(Estoque)
-admin.site.register(Projeto)
-admin.site.register(Historico)
